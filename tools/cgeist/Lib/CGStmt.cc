57ff960bc375dc91a2d35d104b7aa81e4b777ff0
xujing <xujing@smartlogic.com>
2025-09-30 01:57:57 +0000

    for "if", fix to yield out of scope value instead of empty value.

--- tools/cgeist/Lib/CGStmt.cc
+++ tools/cgeist/Lib/CGStmt.cc
@@ -924,9 +924,7 @@ ValueCategory MLIRScanner::VisitIfStmt(clang::IfStmt *stmt) {
           mlir::Value mappedValue = ifMapping.lookup(value);
           yieldValues.push_back(mappedValue);
         } else {
-          mlir::RankedTensorType type = dyn_cast<mlir::RankedTensorType>(elseYieldParams[it.value()].getValue(loc, builder).getType());
-          llvm::SmallVector<mlir::Value, 4> dynamicValues = scope.getDynamicValues(loc, builder, type);
-          yieldValues.push_back(builder.create<mlir::tensor::EmptyOp>(loc, type, dynamicValues));
+          yieldValues.push_back(getBlockArgsInitValues(it.value()).getValue(loc, builder));
         }
       }
       builder.create<scf::YieldOp>(loc, yieldValues);
@@ -947,9 +945,7 @@ ValueCategory MLIRScanner::VisitIfStmt(clang::IfStmt *stmt) {
           mlir::Value mappedValue = elseMapping.lookup(value);
           yieldValues.push_back(mappedValue);
         } else {
-          mlir::RankedTensorType type = dyn_cast<mlir::RankedTensorType>(ifYieldParams[it.value()].getValue(loc, builder).getType());
-          llvm::SmallVector<mlir::Value, 4> dynamicValues = scope.getDynamicValues(loc, builder, type);
-          yieldValues.push_back(builder.create<mlir::tensor::EmptyOp>(loc, type, dynamicValues));
+          yieldValues.push_back(getBlockArgsInitValues(it.value()).getValue(loc, builder));
         }
       }
       builder.create<scf::YieldOp>(loc, yieldValues);
